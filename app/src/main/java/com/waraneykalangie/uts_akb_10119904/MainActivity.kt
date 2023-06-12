bff9c0ba8258473027094757d888e7f48987af44
wkalangie <wkalangie@gmail.com>
2023-06-12 14:15:08 +0700

    first commit

---
+++ app/src/main/java/com/waraneykalangie/uts_akb_10119904/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.waraneykalangie.uts_akb_10119904
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
